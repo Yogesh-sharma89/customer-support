fa2077157592d65c4041471b525c3957ef26941d
Yogesh <nitya.upmanyu@gmail.com>
2026-02-04 20:28:59 +0530

    fix some code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Homepage from "@/pages/home";
 
 const page = async () => {
   const session = await getSession();
+  if(!session?.user?.email){
+    throw new Error('Email not found')
+  }
   return (
     <div>
       <Homepage email={session?.user?.email} />
